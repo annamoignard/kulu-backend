63420404e4aa48b36fd1245b562604e921b18907
Anna Moignard <amoignard.coder@gmail.com>
2021-01-27 13:51:46 +1100

    sign in method seems to be working, no errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def sign_in 
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+        auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+        render json: {username: @user.username, jwt: auth_token.token}, status: 200
+    else
+        render json: {error: "Incorrect Username or Password"}, status: 404 
+
+    end 
+end 
+
   private 
 
   def user_params 
     params.permit(:username, :email, :password, :password_confirmation)
   end 
-end
+  
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     scope '/auth' do
       post "/sign-up", to: "users#create" 
+      post "/sign_in", to: 'users#sign_in'
     end 
 end
