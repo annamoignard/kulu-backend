5757ca78b1f35e10b7b006d7e83f26bda17dadd5
Anna Moignard <amoignard.coder@gmail.com>
2021-01-28 10:31:43 +1100

    updated the params in user controller to include 'auth' as a symbol for password and email on the login form. this allows a jwt to be passed back using the 'find by email' method

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'localhost3001', '127.0.0.1:5050'
+    origins 'localhost:8080'
 
     resource '*',
       headers: :any,
