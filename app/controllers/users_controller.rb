612a4b3368ae9c8e9051c194de182299e25d3dd6
Anna Moignard <amoignard.coder@gmail.com>
2021-01-27 12:50:36 +1100

    generated a users controller, now configuring the create action for sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save 
+      render status: :created
+    else
+      render status: :bad_request
+    end
+  end
+
+  private 
+
+  def user_params 
+    params.permit(:user).permit(:username, :email, :password)
+  end 
 end
