cd0d8e310d0902f5e3e0b907049957441455efb9
Nicole Young <coley504@gmail.com>
2021-01-28 10:45:43 +1100

    added to seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_001056) do
+ActiveRecord::Schema.define(version: 2021_01_27_024827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "date"
+    t.integer "class_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_clients_on_booking_id"
+  end
+
+  create_table "instructors", force: :cascade do |t|
+    t.string "name"
+    t.string "string"
+    t.string "booking_id"
+    t.string "integer"
+    t.bigint "session_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_instructors_on_session_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.date "date"
+    t.string "name"
+    t.string "string"
+    t.integer "instructor_id"
+    t.integer "minutes"
+    t.integer "num_of_people"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -23,4 +60,6 @@ ActiveRecord::Schema.define(version: 2021_01_27_001056) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "clients", "bookings"
+  add_foreign_key "instructors", "sessions"
 end
