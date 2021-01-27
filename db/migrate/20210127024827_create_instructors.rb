2d5b919c0eecd00cd8891483696e412a45b13299
Nicole Young <coley504@gmail.com>
2021-01-27 15:02:12 +1100

    adding the client instructor models again

---
+++ db/migrate/20210127024827_create_instructors.rb
@@ -0,0 +1,13 @@
+class CreateInstructors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :instructors do |t|
+      t.string :name
+      t.string :string
+      t.string :booking_id
+      t.string :integer
+      t.references :session, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
