d3a82577cb8297184e1b7909054f8c32410de2d1
Nicole Young <coley504@gmail.com>
2021-01-27 12:04:28 +1100

    created models booking and session

---
+++ db/migrate/20210127005940_create_sessions.rb
@@ -0,0 +1,14 @@
+class CreateSessions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :sessions do |t|
+      t.date :date
+      t.string :name
+      t.string :string
+      t.integer :instructor_id
+      t.integer :minutes
+      t.integer :num_of_people
+
+      t.timestamps
+    end
+  end
+end
