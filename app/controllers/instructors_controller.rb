4ebfe85648d097866ff75639609dd3c4352b31ac
Anna Moignard <amoignard.coder@gmail.com>
2021-01-28 14:51:37 +1100

    updated seeds file to have seeded instrucotrs for logging into the app with special access

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,5 +1,6 @@
 class InstructorsController < ApplicationController
   before_action :authenticate_user 
+  before_action :authorize_user
 
   def index
   end
@@ -14,5 +15,12 @@ class InstructorsController < ApplicationController
   def destroy
   end 
 
+  private 
+
+  def authorize_user
+    unless user.instructor 
+      render status: :unauthorized
+    end 
+  end  
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_one :instructor
   has_secure_password
   validates :username, presence: true, uniqueness:true
   validates :email, presence: true, uniqueness:true
