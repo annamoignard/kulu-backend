43752506fd05eecca48249e53615d332a23609b1
Nicole Young <coley504@gmail.com>
2021-01-28 14:48:33 +1100

    adding to controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,15 @@
 class BookingsController < ApplicationController
+  def index
+    Bookings = Bookings.all
+    render json: bookings
+  end
+
+  def show
+   
+  end
+  
+  def create
+
+  end
+
 end
