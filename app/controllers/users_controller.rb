ae3fdeb7c25d09835e073268f2f5e473bd50562f
Anna Moignard <amoignard.coder@gmail.com>
2021-01-28 12:31:11 +1100

    changing params on user for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ end
   private 
 
   def user_params 
-    params.permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end 
 
 end 
