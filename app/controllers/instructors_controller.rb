0546b76bbd24578c620f0be46dc78e6895dbeb6e
Anna Moignard <amoignard.coder@gmail.com>
2021-01-29 15:21:46 +1100

    updated logic in sessions controller and created routes for index, show, create and update

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,40 +1,4 @@
 class InstructorsController < ApplicationController
-  before_action :authenticate_user 
   before_action :authorize_user
-
-  #  should this logic be in the sessions controller, or the instructor controller and then i just give access only to instrucotrs?
-  
-
-  def index
-    #  redirect to schedule react component 
-  end
-
-  
-  def create
-    @session = Session.create(session_params)
-    if @session.save 
-      #  good
-    else
-      #  bad
-    end
-  end 
-
-  def update 
-  end 
-
-  def destroy
-  end 
-
-  private 
-
-  def session_params
-    params.require(:session).permit(:data, :time, :name, :minutes)
-  end 
-
-  def authorize_user
-    unless user.instructor 
-      render status: :unauthorized
-    end 
-  end  
 
 end
