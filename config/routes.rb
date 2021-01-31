bb18b7aba7e98afd4470cbad2f066b1eb92f28db
Anna Moignard <amoignard.coder@gmail.com>
2021-01-31 14:41:29 +1100

    added route for status, index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :bookings, only: [:index, :show, :create] 
   resources :sessions, only: [:index, :show, :create, :edit, :destroy]
   resources :instructors, only: [:index, :show, :create, :edit, :destroy]
+  resources :status, only: [:index]
     scope '/auth' do
       post "/sign-up", to: "users#create" 
       post "/sign-in", to: 'users#sign_in'
