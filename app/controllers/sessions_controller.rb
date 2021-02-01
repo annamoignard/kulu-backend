430de5190e869c0ac8e6e33506358d0058bc9c25
Anna Moignard <amoignard.coder@gmail.com>
2021-02-01 12:54:14 +1100

    added functionality for adding, deleting and updating sessions for instructors only

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,33 +1,35 @@
 class SessionsController < ApplicationController
-  before_action :authorize_user, only: [:create, :destroy]
   before_action :set_session, only: [:show, :destroy, :update]
-
-  #  should this logic be in the sessions controller, or the instructor controller and then i just give access only to instrucotrs?
-  
 
   def index
     sessions = Session.all
-    render json: sessions
+    render json: {sessions: sessions, instructor: current_user.try(:instructor)}
   end
 
   def show
     render json:  @session 
   end 
 
-  
   def create
-    # @session = Session.create(session_params)
-    # if @session.save 
-      #  good
-    # else
-      #  bad
-    # end
+    session = Session.new(sessios_params)
+    session.user_id = current_user.id
+    if session.save
+      render status: :created 
+    else
+      render status: :bad_request
+    end
   end 
 
   def update 
+    if @session.update(sessions_params)
+      render status: :no_content
+    else
+      render status: :bad_request
+    end
   end 
 
   def destroy
+    @session.destroy
   end 
 
   private 
@@ -37,13 +39,8 @@ class SessionsController < ApplicationController
   end
 
   def session_params
-    params.require(:session).permit(:data, :time, :name, :minutes)
+    params.require(:session).permit(:data, :time, :name, :minutes, :cost)
   end 
 
-  def authorize_user
-    unless current_user.instructor 
-      render status: :unauthorized
-    end 
-  end  
   
 end
