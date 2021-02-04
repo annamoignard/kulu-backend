e0ffd76211a112c48c4748a15a2a22c9ac77152a
Anna Moignard <amoignard.coder@gmail.com>
2021-02-04 15:12:06 +1100

    added send grid sign up email

---
+++ app/mailers/user_notifier_mailer.rb
@@ -0,0 +1,8 @@
+class UserNotifierMailer < ApplicationMailer
+  default :from => 'amoignard.coder@gmail.com'
+
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email, :subject => 'Thanks for signing up!')
+  end
+end
