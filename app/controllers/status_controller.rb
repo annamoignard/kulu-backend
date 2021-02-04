5b8c3863ecb8b268fdf20c406aae60b2fe85e44f
Anna Moignard <amoignard.coder@gmail.com>
2021-02-04 12:11:03 +1100

    changing status controller for conditional-rendering

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,7 +2,7 @@ class StatusController < ApplicationController
   before_action :authenticate_user
 
   def index
-    render status: :ok
+    render json: {instructor: current_user.instructor}
   end
-  
+
 end
