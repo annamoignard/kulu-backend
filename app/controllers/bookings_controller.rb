102d1a35bc7e65f183b6f1b29883b2fb958b2f54
Anna Moignard <amoignard.coder@gmail.com>
2021-02-09 11:20:00 +1100

    fixing conflicts

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,58 +2,6 @@
 
 class BookingsController < ApplicationController
   before_action :authenticate_user, only: [:index]
-<<<<<<< HEAD
-  before_action :set_booking, only: [:show, :destroy, :update]
-  
-  #client sees bookings they have made. Instructors see client bookings. 
-    def index
-      bookings = current_user.bookings.map do |booking|
-        {
-          id: booking.id,
-          name: booking.session.name,
-          time: booking.session.time,
-          date: booking.session.date,
-          day:  booking.session.day
-        }
-      end
-      render json: {bookings: bookings}
-    end
-    
-
-  #New booking is created 
-    def create
-      session = Session.find_by(name: params[:name], time: params[:time], date: params[:date], day: params[:day])
-      booking = Booking.new(session_id: session.id)
-      booking.user_id = current_user.id
-      if booking.save
-        UserConfirmationMailer.send_confirmation_email(current_user).deliver
-        render status: :created
-      else
-        render status: :unprocessable_entry
-      end
-    end 
-
-  #client can see booking
-    def show
-      booking = Booking.find(params[:id])
-      render json: {
-        name: booking.session.name, 
-        cost: booking.session.cost,
-        day: booking.session.day,
-        time: booking.session.time
-      } 
-    end 
-
-  #client can drop/cancel a booking
-    def destroy
-      @booking.destroy
-    end 
-  
-    private 
-  
-    def set_booking
-      @booking = Booking.find(params[:id])
-=======
   before_action :set_booking, only: %i[show destroy update]
 
   # client sees bookings they have made. Instructors see client bookings.
@@ -66,7 +14,6 @@ class BookingsController < ApplicationController
         date: booking.session.date,
         day: booking.session.day
       }
->>>>>>> 33ab1946e86b9aaf9402fc3cd5f2c4fb85beed35
     end
     render json: { bookings: bookings }
   end
@@ -77,7 +24,7 @@ class BookingsController < ApplicationController
     booking = Booking.new(session_id: session.id)
     booking.user_id = current_user.id
     if booking.save
-      # UserConfirmationMailer.send_confirmation_email(current_user).deliver
+      UserConfirmationMailer.send_confirmation_email(current_user).deliver
       render status: :created
     else
       render json: {errors: booking.errors.full_messages }, status: :unprocessable_entity 
